8534268fdf28405f91d077601924a951470f9aae
wldbs8241 <87288924+wldbs8241@users.noreply.github.com>
2023-02-17 16:15:09 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -0,0 +1,12 @@
+dwarf =[]               
+for _ in range(9):      
+    one = int(input())
+    dwarf.append(one)       
+for i in range(8):         
+    for j in range(i+1, 9): 
+        if sum(dwarf) - (dwarf[i] + dwarf[j]) == 100:  
+            f1, f2 = i, j                               
+            break
+dwarf.pop(f1)       
+dwarf.pop(f2-1)    
+print(*dwarf, sep='\n')
