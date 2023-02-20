b7c45c6000319e1ace70c486f383d68bfb3f03c1
wldbs8241 <87288924+wldbs8241@users.noreply.github.com>
2023-02-20 11:48:19 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -1,12 +1,14 @@
+# 난쟁이 문제 , 런타임 오류
 dwarf =[]               
 for _ in range(9):      
     one = int(input())
-    dwarf.append(one)       
-for i in range(8):         
-    for j in range(i+1, 9): 
-        if sum(dwarf) - (dwarf[i] + dwarf[j]) == 100:  
-            f1, f2 = i, j                               
+    dwarf.append(one) 
+for i in range(8):
+    for j in range(i+1, 9):
+        if sum(dwarf)- (dwarf[i]+dwarf[j]) ==100:
+            f1,f2= i,j
             break
-dwarf.pop(f1)       
-dwarf.pop(f2-1)    
+dwarf.pop(f1)
+dwarf.pop(f2-1)
+           
 print(*dwarf, sep='\n')
