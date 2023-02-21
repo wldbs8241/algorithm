da7baeeea10b849b02ec0792f315a3031146b2d2
wldbs8241 <87288924+wldbs8241@users.noreply.github.com>
2023-02-21 11:32:27 +0900

    [Bronze II] Title: 대표값2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,19 +1,9 @@
-# 평균과 중앙값을 구해보자
 num = []
-n_sum = 0
 for _ in range(5):
     num.append(int(input()))
+avg = int(sum(num)/len(num))
 
-for i in num:
-    n_sum = n_sum + i
-
-for i in range(len(num)-1, 0, -1):
-    for j in range(i):
-        if num[j] > num[j+1]:
-            num[j], num[j+1] = num[j+1], num[j]
-
-avg = int(n_sum / len(num))
-mid = (len(num)//2)
+num.sort()
 
 print(avg)
-print(num[mid])
+print(num[2])
